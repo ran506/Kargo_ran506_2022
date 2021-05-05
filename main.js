a3a962c3ee8a36dbfca2cb2eba68ad94f12d0daf
ranchen506 <ranchen27056@gmail.com>
2021-05-05 21:00:21 +0800

    final version

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log(process.argv.slice(2));
+
+//please run npm i num-words first
+const numWords = require('num-words');
+let input = process.argv.slice(2);
+let output = [];
+input.forEach(i =>  output.push(numWords(i)));
+console.log(output);
